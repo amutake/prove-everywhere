f89e6565bc5fe962ec8bd6d2fc9e10eb4cd66332
amutake <amutake.s@gmail.com>
2014-07-05 22:54:03 +0900

    Return JSON object in terminate coqtop command

--- server/src/ProveEverywhere/Server.hs
+++ server/src/ProveEverywhere/Server.hs
@@ -66,7 +66,7 @@ server coqtopMap seed req respond = handle unknownError $
         res <- withCoqtop coqtopMap n $ \coqtop -> do
             terminateCoqtop coqtop
             delete coqtopMap n
-            return $ responseLBS status200 [] "ok"
+            return $ responseJSON status200 ()
         respond res
 
 fresh :: MVar Int -> IO Int
