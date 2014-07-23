1761a9d0b0d9e2a71a249c106c201353d6076f9a
amutake <amutake.s@gmail.com>
2014-07-23 23:43:09 +0900

    Fill value in summary

--- client/src/jp/ac/titech/itpro/sdl/amutake/proveeverywhere/CoqPreferenceActivity.java
+++ client/src/jp/ac/titech/itpro/sdl/amutake/proveeverywhere/CoqPreferenceActivity.java
@@ -2,9 +2,13 @@ package jp.ac.titech.itpro.sdl.amutake.proveeverywhere;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 import android.view.MenuItem;
 
 public class CoqPreferenceActivity extends Activity {
@@ -32,11 +36,38 @@ public class CoqPreferenceActivity extends Activity {
 		}
 	}
 
-	public static class CoqPreference extends PreferenceFragment {
+	public static class CoqPreference extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			addPreferencesFromResource(R.xml.pref);
+
+			EditTextPreference hostnamePref = (EditTextPreference) findPreference("hostname_key");
+			hostnamePref.setSummary(hostnamePref.getText());
+			EditTextPreference portPref = (EditTextPreference) findPreference("port_key");
+			portPref.setSummary(portPref.getText());
+		}
+
+		@Override
+		public void onResume() {
+			super.onResume();
+			// Set up a listener whenever a key changes
+			getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+		}
+
+		@Override
+		public void onPause() {
+			super.onPause();
+			// Set up a listener whenever a key changes
+			getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+		}
+
+		@Override
+		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+			Log.d("pref", "pref changed: " + key);
+			String value = sharedPreferences.getString(key,  null);
+			Preference pref = findPreference(key);
+			pref.setSummary(value);
 		}
 	}
 }
