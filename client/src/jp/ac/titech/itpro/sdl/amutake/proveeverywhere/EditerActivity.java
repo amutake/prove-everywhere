b8264fb420a04c85c60af8b573d1613e236fe31b
amutake <amutake.s@gmail.com>
2014-07-24 22:08:13 +0900

    Save code

--- client/src/jp/ac/titech/itpro/sdl/amutake/proveeverywhere/EditerActivity.java
+++ client/src/jp/ac/titech/itpro/sdl/amutake/proveeverywhere/EditerActivity.java
@@ -8,9 +8,11 @@ import com.android.volley.Response.Listener;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -22,7 +24,7 @@ import android.widget.TextView;
 
 public class EditerActivity extends Activity {
 
-	private int codeId; // if this is -1, it means new code.
+	private long codeId; // if this is -1, it means new code.
 	private String codeName;
 	private String codeContent;
 
@@ -36,6 +38,8 @@ public class EditerActivity extends Activity {
 	private Button backButton;
 	private Button gotoButton;
 	private Button restartButton;
+
+	private SQLiteDatabase db;
 
 	private final static String CODE_KEY = "CODE_KEY";
 	private final static String PROOF_STATE_KEY = "PROOF_STATE_KEY";
@@ -51,9 +55,12 @@ public class EditerActivity extends Activity {
 		setContentView(R.layout.activity_editer);
 
 		Intent i = getIntent();
-		codeId = i.getIntExtra(Strings.codeId, -1);
+		codeId = i.getLongExtra(Strings.codeId, -1);
 		codeName = i.getStringExtra(Strings.codeName);
 		codeContent = i.getStringExtra(Strings.codeContent);
+
+		CoqCodeDbOpenHelper helper = new CoqCodeDbOpenHelper(this);
+		db = helper.getWritableDatabase();
 
 		View codeAreaWrapper = findViewById(R.id.code_area_wrapper);
 		View infoAreaWrapper = findViewById(R.id.info_area_wrapper);
@@ -114,10 +121,25 @@ public class EditerActivity extends Activity {
 		int id = item.getItemId();
 		switch (id) {
 		case android.R.id.home:
+			updateCoqCode();
 			finish();
 			return true;
 		default:
 			return super.onMenuItemSelected(featureId, item);
+		}
+	}
+
+	private void updateCoqCode() {
+		ContentValues values = new ContentValues();
+		values.put(CoqCodeColumns.CODE, codeArea.getText().toString());
+		if (codeId == -1) {
+			values.put(CoqCodeColumns.NAME, "hoge.v");
+			db.insert(CoqCodeColumns.TBNAME, "", values);
+		} else {
+			String whereClause = CoqCodeColumns._ID + " = ?";
+			String whereArgs[] = new String[1];
+			whereArgs[0] = Long.toString(codeId);
+			db.update(CoqCodeColumns.TBNAME, values, whereClause, whereArgs);
 		}
 	}
 
