461b1aa4f748ff774357bc5a6fbce7d96b886539
amutake <amutake.s@gmail.com>
2014-08-31 19:51:57 +0900

    Add ExceededMaxProcsError into ErrorType.java

--- android/src/jp/ac/titech/itpro/sdl/amutake/proveeverywhere/ErrorType.java
+++ android/src/jp/ac/titech/itpro/sdl/amutake/proveeverywhere/ErrorType.java
@@ -1,5 +1,5 @@
 package jp.ac.titech.itpro.sdl.amutake.proveeverywhere;
 
 public enum ErrorType {
-	NoSuchCoqtopError, PromptParseError, RequestParseError, NoSuchApiError, UnknownError;
+	NoSuchCoqtopError, PromptParseError, RequestParseError, NoSuchApiError, UnknownError, ExceededMaxProcsError;
 }
